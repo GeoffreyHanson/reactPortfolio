ec9a513363ee104e5936760b102eb7c7d12cee01
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-01-03 21:06:43 -0600

    Removed package-lock and reinstalled modules with yarn. Removed /build from gitignore.

---
+++ build/precache-manifest.a01c370439872f88fa201c8ff21fcbc7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c80c3b88505d729f376e932041a76ff",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "d8c541bf27dc21f3c43a",
+    "url": "/reactPortfolio/static/css/main.e404413a.chunk.css"
+  },
+  {
+    "revision": "df3ad20234e43db0eff6",
+    "url": "/reactPortfolio/static/js/2.c43b84a0.chunk.js"
+  },
+  {
+    "revision": "d8c541bf27dc21f3c43a",
+    "url": "/reactPortfolio/static/js/main.d0a7af84.chunk.js"
+  },
+  {
+    "revision": "17474da84baf82edfe59",
+    "url": "/reactPortfolio/static/js/runtime-main.90378ba6.js"
+  },
+  {
+    "revision": "79dc10019d3019898c27cb64e433f555",
+    "url": "/reactPortfolio/static/media/BaselineThumb.79dc1001.png"
+  },
+  {
+    "revision": "93145a98f779fa7ec6ff2502b80f5e2a",
+    "url": "/reactPortfolio/static/media/BitcoinWalletThumb.93145a98.jpg"
+  },
+  {
+    "revision": "09fb2ddc8e92456f95bd3dea85f6a8ae",
+    "url": "/reactPortfolio/static/media/BookSaverThumb.09fb2ddc.JPG"
+  },
+  {
+    "revision": "d1f9039e187f2b53faece73cd8fb4938",
+    "url": "/reactPortfolio/static/media/FG-site-thumb.d1f9039e.png"
+  }
+]);
