2fc6a91f081ba2f125f1cb69406d471f0913b57e
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-01-04 16:44:05 -0600

    New build

---
+++ build/precache-manifest.bd64720e5bbec708a63598ba3056eaa4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6936e0c999db8ff0199a27c06d92b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc4b3a7a936a9bb6070",
+    "url": "/static/css/main.e404413a.chunk.css"
+  },
+  {
+    "revision": "df3ad20234e43db0eff6",
+    "url": "/static/js/2.c43b84a0.chunk.js"
+  },
+  {
+    "revision": "4cc4b3a7a936a9bb6070",
+    "url": "/static/js/main.257a318b.chunk.js"
+  },
+  {
+    "revision": "6d60b4ae05f839f97de9",
+    "url": "/static/js/runtime-main.b9740393.js"
+  },
+  {
+    "revision": "79dc10019d3019898c27cb64e433f555",
+    "url": "/static/media/BaselineThumb.79dc1001.png"
+  },
+  {
+    "revision": "93145a98f779fa7ec6ff2502b80f5e2a",
+    "url": "/static/media/BitcoinWalletThumb.93145a98.jpg"
+  },
+  {
+    "revision": "09fb2ddc8e92456f95bd3dea85f6a8ae",
+    "url": "/static/media/BookSaverThumb.09fb2ddc.JPG"
+  },
+  {
+    "revision": "d1f9039e187f2b53faece73cd8fb4938",
+    "url": "/static/media/FG-site-thumb.d1f9039e.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactPortfolio/precache-manifest.a01c370439872f88fa201c8ff21fcbc7.js"
+  "/precache-manifest.bd64720e5bbec708a63598ba3056eaa4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
